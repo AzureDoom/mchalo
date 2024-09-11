7a54d288500fc38180c8e5b3a5eac69995fa6ae1
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-11 11:49:43 -0400

    Guns no longer animate in the inventory unless sword.

--- common/src/main/java/mod/azure/mchalo/client/models/GunModel.java
+++ common/src/main/java/mod/azure/mchalo/client/models/GunModel.java
@@ -2,11 +2,23 @@ package mod.azure.mchalo.client.models;
 
 import mod.azure.azurelib.common.api.client.model.DefaultedItemGeoModel;
 import mod.azure.azurelib.common.api.common.animatable.GeoItem;
+import mod.azure.azurelib.common.internal.common.constant.DataTickets;
+import mod.azure.azurelib.core.animation.AnimationState;
+import mod.azure.mchalo.item.EnergySwordItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 public class GunModel<T extends Item & GeoItem> extends DefaultedItemGeoModel<T> {
     public GunModel(ResourceLocation assetSubpath) {
         super(assetSubpath);
+    }
+
+    @Override
+    public void setCustomAnimations(T animatable, long instanceId, AnimationState<T> animationState) {
+        if (!(animatable instanceof EnergySwordItem))
+            switch (animationState.getData(DataTickets.ITEM_RENDER_PERSPECTIVE)) {
+                case GUI, GROUND, HEAD, NONE, FIXED -> animationState.getController().stop();
+            }
+        super.setCustomAnimations(animatable, instanceId, animationState);
     }
 }
